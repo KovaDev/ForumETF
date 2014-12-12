5ad876c348ea83fb558dc3adb35bac3dae6f5603
KovaDev <kova.dev@outlook.com>
2014-12-12 03:09:40 +0100

    Dodat paket System.Web.Optimization (bundles)

--- ForumETF/Global.asax.cs
+++ ForumETF/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
+using ForumETF.App_Start;
 
 namespace ForumETF
 {
@@ -13,6 +15,7 @@ namespace ForumETF
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles); // dodato naknadno
         }
     }
 }
